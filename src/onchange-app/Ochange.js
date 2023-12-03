a109dc5a4b92ab1e7b0235abdf6d53716df2e06b
Nageshwar Reddy Pandem <nageshwar.uideveloper@gmail.com>
2023-12-03 14:38:11 +0800

    dec 3rd

--- src/onchange-app/Ochange.js
+++ src/onchange-app/Ochange.js
@@ -6,16 +6,23 @@ import '../onchange-app/onchange.css';
 import 'bootstrap/dist/css/bootstrap.css'; 
 const Ochange = () => {
     const [inputValue, setInputValue] = useState("");
-      const  handleInputChange = e=>{
-        setInputValue(e.target.value);
+    const [data,setdata] = useState([]);
+      const  handleInputChange = (e)=>{
+        let val1= data;
+        val1.push({
+          name:e
+        })
+        setdata(val1);
+        console.log(val1,"data");
       }
   return (
     <div className='container' style={{backgroundColor:'lightblue',height:'40rem',width:'80rem'}}> 
       <div className='card-text'>
-         <h1 className='text' style={{textAlign:'center'}}>React App</h1>
+         <h2 className='text' style={{textAlign:'center'}}>React App</h2>
       </div>
       <div className='inputtext'>
-        <InputText value={inputValue} onChange={handleInputChange}></InputText>
+        <InputText value={inputValue} onChange={(e)=>{setInputValue(e.target.value)}}></InputText>
+        <Button style={{borderRadius:'3px',padding:'2px'}} onClick={()=>{handleInputChange(inputValue)}}>Add</Button>
       </div>
      </div>
   )
