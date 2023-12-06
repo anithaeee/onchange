f09f9089f5bab50e5ce64ef27fb86cf38dd65f27
Nageshwar Reddy Pandem <nageshwar.uideveloper@gmail.com>
2023-12-06 22:45:17 +0800

    dec 6th

--- src/onchange-app/Ochange.js
+++ src/onchange-app/Ochange.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Button } from 'primereact/button';
 import { InputText } from 'primereact/inputtext';
 import { useState } from 'react';
@@ -7,6 +7,9 @@ import 'bootstrap/dist/css/bootstrap.css';
 const Ochange = () => {
     const [inputValue, setInputValue] = useState("");
     const [data,setdata] = useState([]);
+    useEffect(()=>{
+      console.log(data)
+    },[data])
       const  handleInputChange = (e)=>{
         let val1= data;
         val1.push({
@@ -23,6 +26,27 @@ const Ochange = () => {
       <div className='inputtext'>
         <InputText value={inputValue} onChange={(e)=>{setInputValue(e.target.value)}}></InputText>
         <Button style={{borderRadius:'3px',padding:'2px'}} onClick={()=>{handleInputChange(inputValue)}}>Add</Button>
+      </div>
+      <div className='table'> 
+        <table>
+          <thead>
+          <tr>
+            <th>Id</th>
+            <th>Name</th>
+          </tr>
+          </thead>
+          <tbody>
+          {data.map((dt,i)=>{
+            return(
+              <tr>
+                <td>{i+1}</td>
+               <td>{dt.name}</td>
+          </tr>
+            )
+            
+          })}
+          </tbody>
+        </table>
       </div>
      </div>
   )
